8af5ad4391dedc9d4f6f1bb9f8c746f6a33fbdf7
Max Jaecklein <maximilian.jaecklein@tum.de>
2025-08-04 23:57:08 +0000

    .

--- drivers/misc/disagg/mmio.c
+++ drivers/misc/disagg/mmio.c
@@ -7,6 +7,12 @@
 
 static struct mmio_message *msg;
 
+static u64 agg_read = 0;
+static u64 agg_write = 0;
+
+//static u64 max = 100000;
+//static u64 ctr = 0;
+
 int mmio_read(u64 size, u64 addr, unsigned long *val)
 {
 ktime_t start, end;
@@ -23,16 +29,27 @@ ktime_t start, end;
 	// First byte of buf is reserved for unencrypted OP_TYPE
 	msg->op = DISAGG_DEV_OP_READ;
 
-//start = ktime_get();
+start = ktime_get();
 	ivshmem_mmio_region_write(msg, (sizeof(*msg) - sizeof(msg->value)));
-//end = ktime_get();
+end = ktime_get();
+agg_write += (u64) ktime_to_ns(end) - (u64) ktime_to_ns(start);
 //pr_info("time measured pie: Shmem write;%llu;%llu end\n", size, (u64) ktime_to_ns(end) - (u64) ktime_to_ns(start));
 
 start = ktime_get();
 	ivshmem_mmio_region_read(val, sizeof(msg->value));
 end = ktime_get();
-pr_info("time measured pie: Shmem read;%llu;%llu end\n", size, (u64) ktime_to_ns(end) - (u64) ktime_to_ns(start));
+agg_read += (u64) ktime_to_ns(end) - (u64) ktime_to_ns(start);
+//pr_info("time measured pie: Shmem read;%llu;%llu end\n", size, (u64) ktime_to_ns(end) - (u64) ktime_to_ns(start));
 
+#if 0
+if (++ctr == max) {
+pr_info("time measured pie: Shmem write;%llu;%llu end\n", size, agg_write / max);
+pr_info("time measured pie: Shmem read;%llu;%llu end\n", size, agg_read / max);
+ctr = 0;
+agg_read = 0;
+agg_write = 0;
+}
+#endif
 	return 0;
 }
 
